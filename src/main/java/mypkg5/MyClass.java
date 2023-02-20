4f0b53af932695429348fefbd484b7e580efed38
Rogger08 <ashishbansal.ab.08@gmail.com>
2023-02-20 10:25:32 +0530

    first commit

---
+++ src/main/java/mypkg5/MyClass.java
@@ -0,0 +1,13 @@
+package mypkg5;
+public class MyClass {
+	public void show()
+	{
+		System.out.println("Hello DevOps! Welcome to Jenkins");
+	}
+	public static void main(String[] args) {
+
+		MyClass ob = new MyClass();
+		ob.show();
+	}
+
+}
